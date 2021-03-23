a3f4452e693391a0b35cdf097f027049a51af428
dwisecar <dwisecar@gmail.com>
2021-03-23 10:58:56 -0700

    graph deletes

--- graph.py
+++ graph.py
@@ -29,6 +29,24 @@ class Graph:
     for node in list(self.graph.keys()):
       edge_list.append("{}: {}".format(node, list(self.graph[node].keys())))
     return edge_list
+  
+  def delete_node(self, node):
+    if node not in list(self.graph.keys()):
+      raise KeyError("Node not in graph")
+    del self.graph[node]
+    for key in list(self.graph.keys()):
+      if key in list(self.graph[key].keys()):
+        del self.graph[key][node]
+
+  def delete_edge(self, node1, node2):
+    if node2 in list(self.graph[node1].keys()) and self.graph[node2]:
+      del self.graph[node1][node2]
+    elif node1 not in list(self.graph.keys()) or node2 not in list(self.graph.keys()):
+      raise KeyError("Node not in graph")
+    else:
+      raise ValueError("No edge for these nodes")
+
+
 
 dummy = Graph()
 dummy.add_edge(1, 2, 2)
@@ -39,5 +57,8 @@ dummy.add_edge(4, 6, 6)
 dummy.add_edge(5, 6, 5)
 dummy.add_edge(7, 4)
 
+print(dummy.nodes())
+print(dummy.edges())
+dummy.delete_node(7)
 print(dummy.nodes())
 print(dummy.edges())
